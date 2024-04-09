80478f415dd47e8aae8dac99cee34006c3760b0e
usmonjon <umediagram@gmail.com>
2024-04-09 15:19:58 +0500

    Figmaland project navbar setup

---
+++ script.js
@@ -0,0 +1,13 @@
+/* ********
+
+    Author: Usmonjon Hasanov
+    Telegram: @DevMERN
+    Instagram: @usmonjon_hasanov
+    Facebook: @usmonjonh
+    Twitter (X): @usmonjon_hasanov
+    LinkedIn: /in/usmonjon_hasanov
+    WhatsApp: +998-(90)-193-1320
+    Tel: +998-(90)-193-1320
+    
+    ******** */
+"use strict";
